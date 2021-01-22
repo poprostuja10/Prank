93547dfd9a085a98d08ad18e3f5d10421a4b302b
poprostuja10 <77839537+poprostuja10@users.noreply.github.com>
2021-01-22 13:31:34 +0100

    Add files via upload
    Po włączeniu pliku, otworzy się konsola programisty w której będą się wyświetlały różne napisy o hackowaniu. Na koniec komputer się wyłączy. W 1000% bezpieczne.

---
+++ Gra.bat
@@ -0,0 +1,37 @@
+@echo off
+color a
+@timeout 2
+tree
+cls
+echo naurgoingb9823t72n09thn3gjern89403ytnjjeuiorgg_43t34TGA_346ytq46ytt43qT$_6y3yfwaertgWATGaT^$Y_
+@timeout 1
+cls
+echo fwrhaht4bhtq2+N6tn3 q4_TGRWAJT%$^NJ_^HBA$TNJ^_HN^T&FGEA_GHEARTGAGEARHN_*&#436_63q
+@timeout 1
+cls
+echo bufawehyburyg8792b874q1bh_ewanfhuoiew87yGTt43-098u4j3yhb_fewa
+@timeout 1
+cls
+echo bhugiaebfyuiaw3890634q_BGUIfu89403w2890-5jytmnu0rdesthna4t5aW5879Q75GB74 TQb_tHB43QHBAW4nt$n$tbw_
+@timeout 1
+cls
+echo Processing...
+@timeout 5
+cls
+color 70
+echo Uploading a file "compdestroy"
+@timeout 1
+cls
+color 07
+echo Uploading a file "compdestroy"
+@timeout 1
+cls
+color 70
+echo Uploading a file "compdestroy"
+@timeout 1
+cls
+color 07
+echo Uploading a file "compdestroy"
+@timeout 1
+cls
+shutdown/p
